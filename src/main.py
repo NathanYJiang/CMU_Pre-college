e74c3b6f45ada5bfc2fd6ee77dad85bd5998bef3
Nathan <108350233+NathanYJiang@users.noreply.github.com>
2023-08-01 16:32:33 -0400

    colors :0

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ def onAppStart(app):
     app.s = 70
     app.startX = 0
     app.startY = 300
+    app.colors = [rgb(237, 1, 1), rgb(15, 152, 245), rgb(246, 247, 239), 
+                  rgb(246, 139, 46)]
     restart(app)
 
 
@@ -15,13 +17,17 @@ def restart(app):
 
 def redrawAll(app):
     app.board.draw(app)
-    app.board.drawSettlement(*getHexCoords(app, 8, 1), 'red')
-    app.board.drawCity(*getHexCoords(app, 8, 2), 'blue')
+    app.board.drawSettlement(*getHexCoords(app, 8, 1), fill=app.colors[0])
+    app.board.drawCity(*getHexCoords(app, 8, 2), fill=app.colors[1])
     print(len(app.board.centers))
     for x, y in app.board.midpoints:
         drawCircle(*getHexCoords(app, x, y), 5, fill='magenta')
     for x, y in app.board.ports:
         drawCircle(*getHexCoords(app, x, y), 5, fill='magenta')
+    drawCircle(10, 10, 10, fill=app.colors[0])
+    drawCircle(10, 30, 10, fill=app.colors[1])
+    drawCircle(10, 50, 10, fill=app.colors[2])
+    drawCircle(10, 70, 10, fill=app.colors[3])
 
 
 runApp(width=1400, height=850)
