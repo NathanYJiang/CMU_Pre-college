653f7b4155c703c7ce873874dcfaeea33b4cf63d
Nathan <108350233+NathanYJiang@users.noreply.github.com>
2023-08-05 17:16:10 -0400

    cmu_graphics stuff

--- src/utils/images.py
+++ src/utils/images.py
@@ -1,3 +1,5 @@
+import os
+import pathlib
 from cmu_graphics import CMUImage
 from PIL import Image, ImageFilter, ImageEnhance
 
@@ -8,13 +10,17 @@ from PIL import Image, ImageFilter, ImageEnhance
 # PIL sharpen learned from: https://pythonexamples.org/python-pillow-image-sharpen/
 # used https://tinypng.com/ to minimize images
 
+# from Ray's cmu_graphics demo
+def getImagePath(imageName):
+    return os.path.join(pathlib.Path(__file__).parent, f"../images/{imageName}")
+
 def getImages(app):
     # tokens
     app.tokens = dict()
     for i in range(2, 13):
         # 7 is the robber!
         if i == 7: continue
-        token = Image.open(f'images/token{i}.webp')
+        token = Image.open(getImagePath(f'token{i}.webp'))
         token.thumbnail((50, 50))
         token = token.filter(ImageFilter.SHARPEN)
         app.tokens[i] = CMUImage(token)
@@ -22,30 +28,31 @@ def getImages(app):
     # tiles
     app.tiles = dict()
     for resource in ['desert', 'field', 'pasture', 'forest', 'hill', 'mountain']:
-        tile = Image.open(f'images/{resource}.png')
+        tile = Image.open(getImagePath(f'{resource}.png'))
         tile.thumbnail((140, 140))
         app.tiles[resource] = CMUImage(tile)
     
     # dice
     app.dice = dict()
     for i in range(1, 7):
-        dice = Image.open(f'images/dice{i}.png')
+        dice = Image.open(getImagePath(f'dice{i}.png'))
         dice.thumbnail((75, 75))
         app.dice[i] = CMUImage(dice)
     
     # player icon
     app.icons = []
     for i in range(app.numPlayers):
-        icon = Image.open(f'images/p{i+1}icon.png')
+        icon = Image.open(getImagePath(f'p{i+1}icon.png'))
         icon.thumbnail((60, 60))
         app.icons.append(CMUImage(icon))
 
     # resources
     app.resImages = dict()
     for resource in app.resources:
-        resImage = Image.open(f'images/resources--{resource}.png')
+        resImage = Image.open(getImagePath(f'resources--{resource}.png'))
+        resImage=resImage.convert('RGBA')
         resImage.thumbnail((50, 80))
-        #resImage = resImage.filter(ImageFilter.SHARPEN)
+        resImage = resImage.filter(ImageFilter.SHARPEN)
         app.resImages[resource] = CMUImage(resImage)
 
 
